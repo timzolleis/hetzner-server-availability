5912a11deed958e970ce500b187e5b8d110dcb24
Tim Zolleis <tim@zolleis.net>
2024-10-14 17:00:00 +0200

    remove cf packages

--- app/routes/api.trigger.ts
+++ app/routes/api.trigger.ts
@@ -1,4 +1,4 @@
-import {json} from "@remix-run/react";
+import {json} from "@remix-run/node";
 import {getLastAvailabilityLog} from "~/models/availability-log.server";
 import {runAbilityCheck} from "~/lib/availability-check.server";
 import {env} from "~/lib/env.server";
